f05141b057d1b373cce12a6f4e8a5c87204d39dd
MIRiyA <miriya.lee@gmail.com>
2015-02-12 22:38:50 +0900

    usd

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -4,7 +4,7 @@ Spree::Variant.class_eval do
 
   def to_hash
     price = self.prices.find_by(currency: Spree::Config[:presentation_currency]).display_price.to_html
-    price_usd = Spree::Money.new(self.price.to_i, { currency: "USD" }).to_html
+    price_usd = self.prices.find_by(currency: "USD").display_price.to_html
     old_price = self.product.display_html_old_price
     old_price_usd = Spree::Money.new(self.old_price.to_i, { currency: "USD" }).to_html
     {
