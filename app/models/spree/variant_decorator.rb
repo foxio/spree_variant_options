2b3af671def7f5102195e2df36de2788da2a486e
MIRiyA <miriya.lee@gmail.com>
2015-04-13 16:38:54 +0900

    get rid of find_by KRW

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -3,11 +3,12 @@ Spree::Variant.class_eval do
   include ActionView::Helpers::NumberHelper
 
   def to_hash
-    price = self.prices.find_by(currency: Spree::Config[:presentation_currency]).display_price.to_html
-    price_usd = self.prices.find_by(currency: "USD").display_price.to_html
+    usd = self.prices.find_by(currency: "USD")
+    price = display_currency(usd)
+    price_usd = usd.display_price.to_html
     if self.product.old_price
+      old_price_usd = display_currency(self.product.old_price)
       old_price = display_currency(self.product.old_price, to: 'KRW')
-      old_price_usd = display_currency(self.product.old_price, to: 'USD')
     end
     {
       :id => self.id,
